2230a8aebca84e77381b45ac9378e17f5d3a6ea2
ArtuZeraMusic <artur.silvestre88@gmail.com>
2022-03-08 20:53:34 -0300

    Movendo Peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,48 +1,47 @@
 package boardgame;
 
 public class Board {
-	
+
 	private int rows;
 	private int columns;
-	private Piece [][] pieces;
-	
-	
+	private Piece[][] pieces;
+
 	public Board(int rows, int columns) {
-		//SE a quantidade de linhas for < 1 OU a quantidade de Colunas <1;
-		if(rows < 1 || columns < 1) {
+		// SE a quantidade de linhas for < 1 OU a quantidade de Colunas <1;
+		if (rows < 1 || columns < 1) {
 			throw new BoardException("Erro Criando Tabuleiro: Precisa ter ao menos 1 linha e 1 coluna");
 		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
-				
+
 	}
 
 	public int getRows() {
 		return rows;
 	}
 
-
 	public int getColumns() {
 		return columns;
 	}
 
-	//public retornando um método do tipo Piece com o nome piece, recebendo int row e collumn
-	
-	public Piece piece (int row, int column) {
-		if(!positionExists(row, column)) {
+	// public retornando um método do tipo Piece com o nome piece, recebendo int row
+	// e collumn
+
+	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
 			throw new BoardException("Posição fora do Tabuleiro");
 		}
 		return pieces[row][column];
 	}
 
 	public Piece piece(Position position) {
-		if(!positionExists(position)) {
+		if (!positionExists(position)) {
 			throw new BoardException("Posição fora do Tabuleiro");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
 			throw new BoardException("Já existe uma peça na posição " + position);
@@ -50,19 +49,32 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
-	
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posição fora do Tabuleiro");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
-	
+
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
+
 	public boolean thereIsAPiece(Position position) {
-		if(!positionExists(position)) {
+		if (!positionExists(position)) {
 			throw new BoardException("Posição fora do Tabuleiro");
 		}
 		return piece(position) != null;
 	}
 }
-
