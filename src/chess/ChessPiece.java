5da42041740e85128a673b5b98ad0a3bd98cdfc3
ArtuZeraMusic <artur.silvestre88@gmail.com>
2022-03-25 20:50:46 -0300

    Cavalo

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -34,10 +34,9 @@ public abstract class ChessPiece extends Piece {
 		return ChessPosition.fromPosition(position);
 	}
 
-	protected boolean isThereOpponentPiece(Position postion) {
+	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
-	
 
 }
